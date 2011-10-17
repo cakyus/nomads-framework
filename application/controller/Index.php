fccfa13b0491c776667b15512da186f5e64519dd
cakyus <cakyus@gmail.com>
2011-10-17 21:27:32 +0700

    simple test to check whether everything is okay

--- application/controller/Index.php
+++ application/controller/Index.php
@@ -7,8 +7,6 @@ class Index_Controller extends Nomads_Controller {
 	}
 	
 	public function index() {
-		$this->view->set('documentTitle', 'Welcome');
-		$this->view->set('pageTitle', 'Hello');
-		$this->view->show('index.php');
+		echo 'It\'s works !';
 	}
 }
